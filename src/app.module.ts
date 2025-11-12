6109ce35bb44fb8f0b24355112650c5cd2676015
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-11-11 21:15:34 -0300

    feat(account): create account controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service.js';
+import { CreateAccountController } from './controllers/create-account.controller.js';
 
 @Module({
+  controllers: [
+    CreateAccountController
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
